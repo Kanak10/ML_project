06f4cfc6696a3c22c7761ffad364932e07dbfb2f
kanakgems4 <kanak.gupta@gemsessence.com>
2026-02-09 22:39:27 +0530

    initial setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+# Responsible for creating my ML application as a package
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this funxtion will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Kanak",
+    author_email="kanakgupta814@gmail.com",
+    packages=find_packages(),
+    requires=get_requirements('requirements.txt'),
+)
